abc4f7dae9a9009a67d5afa8a592c75e69e2ba99
korious <innovativepotencial@gmail.com>
2025-02-03 12:53:46 +0100

    CORS

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import math
 import requests
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Helper Functions
 def is_prime(n):
@@ -87,4 +90,4 @@ def after_request(response):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
